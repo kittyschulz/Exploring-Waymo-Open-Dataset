e2977ed1407648f8b209003f6b65689cd8f76ebf
Katerina Schulz <katerinaschulz@Katerinas-MacBook-Pro.local>
2019-09-10 07:58:59 -0700

    updated functions

--- frames_to_Pandas.py
+++ frames_to_Pandas.py
@@ -3,14 +3,35 @@ import itertools
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+import re
 
-def frames_to_Pandas(waymo_frame):
-  """
-  inputs: takes single waymo Frame proto.
-  outputs: adds rows to existing Pandas DataFrame
-
-  """
-  frame_string = frame_string[frame_string.find('stats')+5:].replace(' ','').replace('\n','')
-  pass
+def extract_frame_data(frame):
+  frame_data = {                     
+       'time_of_day': frame.context.stats.time_of_day,
+       'location': frame.context.stats.location,
+       'weather': frame.context.stats.weather
+    }
+  object_counts = {object_type_name(x.type): x.count for x in frame.context.stats.laser_object_counts}
+  frame_data.update(object_counts)
   
+  object_count = collections.Counter([object_type_name(x.type) for x in frame.laser_labels])
+  print(object_count)
+  
+  return frame_data
 
+
+frame_df = pd.DataFrame()
+FILENAME = '/content/waymo-od/tutorial/frames'
+dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
+for data in dataset:
+    frame = open_dataset.Frame()
+    print('Parsing frame')
+    frame.ParseFromString(bytearray(data.numpy()))
+    frame_data = extract_frame_data(frame)
+    frame_df = frame_df.append(frame_data, ignore_index=True)
+
+frame_df.head()
+
+
+def object_type_name(x):
+  return label_pb2.Label.Type.Name(x)

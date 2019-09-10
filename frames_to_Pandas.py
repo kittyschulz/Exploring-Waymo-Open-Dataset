9d27ce57805fb599e8dfe5cadaebd1917d2ff9a9
Katerina Schulz <katerinaschulz@Katerinas-MacBook-Pro.local>
2019-09-10 08:00:11 -0700

    removed print from funcs

--- frames_to_Pandas.py
+++ frames_to_Pandas.py
@@ -15,7 +15,7 @@ def extract_frame_data(frame):
   frame_data.update(object_counts)
   
   object_count = collections.Counter([object_type_name(x.type) for x in frame.laser_labels])
-  print(object_count)
+  #print(object_count)
   
   return frame_data
 
@@ -25,13 +25,12 @@ FILENAME = '/content/waymo-od/tutorial/frames'
 dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
 for data in dataset:
     frame = open_dataset.Frame()
-    print('Parsing frame')
+    #print('Parsing frame')
     frame.ParseFromString(bytearray(data.numpy()))
     frame_data = extract_frame_data(frame)
     frame_df = frame_df.append(frame_data, ignore_index=True)
 
 frame_df.head()
-
 
 def object_type_name(x):
   return label_pb2.Label.Type.Name(x)
